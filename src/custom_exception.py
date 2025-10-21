e4e7d21c516b3516c2dad1f561e60513355f4f68
Aoron seby <aoron@Aorons-MacBook-Air.local>
2025-10-21 18:42:49 +0300

    commit

--- src/custom_exception.py
+++ src/custom_exception.py
@@ -1,17 +1,20 @@
-import traceback
 import sys
+import traceback
 
 class CustomException(Exception):
-    def __init__(self, error_message, error_detail:sys):
+    def __init__(self, error_message):
         super().__init__(error_message)
-        self.error_message = self.get_detailed_error_message(error_message, error_detail)
+        self.error_message = self.get_detailed_error_message(error_message)
 
     @staticmethod
-    def get_detailed_error_message(error_message, error_detail:sys):
-        exc_tb = error_detail.exc_info()
-        file_name = exc_tb.tb_frame.f_code.co_filename
-        line_number = exc_tb.tb_lineno
-        return f"Error in ({file_name}), line ({line_number}): {error_message}"
+    def get_detailed_error_message(error_message):
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        if exc_tb:
+            file_name = exc_tb.tb_frame.f_code.co_filename
+            line_number = exc_tb.tb_lineno
+            return f"Error [{exc_type.__name__}] in ({file_name}), line ({line_number}): {error_message}"
+        else:
+            return f"Error: {error_message}"
 
     def __str__(self):
         return self.error_message
